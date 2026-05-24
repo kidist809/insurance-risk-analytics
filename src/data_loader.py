0a84f874cf843eda44d304719f0d31290c3f4be3
Kidist Mekuria <kidimeku2@gmail.com>
2026-05-24 22:44:41 +0300

    complete TAsk 1 EDA and intrim report

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,8 @@
- 
+import pandas as pd
+
+def load_data(path):
+    """
+    Load insurance dataset
+    """
+    df = pd.read_csv(path)
+    return df 
--- src/eda_utils.py
+++ src/eda_utils.py
@@ -1,1 +1,32 @@
- 
+import pandas as pd
+
+
+def check_missing(df):
+    return df.isnull().sum()
+
+
+def loss_ratio(df):
+    return (
+        df["TotalClaims"].sum()
+        / df["TotalPremium"].sum()
+    )
+
+
+def create_margin(df):
+
+    df["Margin"] = (
+        df["TotalPremium"]
+        - df["TotalClaims"]
+    )
+
+    return df
+
+
+def summarize(df):
+
+    return df.describe()
+
+
+def datatype_info(df):
+
+    return df.dtypes 
